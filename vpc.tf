ef39611901c9f9e9a98c48cf2d423101e938d250
David Jones <tafftoo@gmail.com>
2016-02-02 11:51:13 +0000

    Add name to VPC

--- vpc.tf
+++ vpc.tf
@@ -2,6 +2,9 @@
 resource "aws_vpc" "default" {
   cidr_block = "10.0.0.0/16"
   enable_dns_support = true
+  tags {
+    Name = "${var.env}-vpc"
+  }
 }
 
 # Create an internet gateway to give our subnet access to the outside world
