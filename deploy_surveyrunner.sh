72debb40e08ccb1db1d038d61a68d23a7c70a152
Dan Hilton <daniel.hilton@gmail.com>
2016-02-02 13:25:57 +0000

    Fix to specify the correct github repo to be cloned for install.

--- deploy_surveyrunner.sh
+++ deploy_surveyrunner.sh
@@ -3,14 +3,14 @@
 # Assumes
 # Call this script with the AWS elasticbeanstalk app name and env name
 
-AUTHOR_REPO_URL=https://github.com/ONSdigital/eq-author
+SURVEYRUNNER_REPO_URL=https://github.com/ONSdigital/eq-survey-runner
 BRANCH=master
-pip install --user ebcli==3.7.2
+pip install --user awsebcli
 
-echo $AUTHOR_REPO_URL
+echo $SURVEYRUNNER_REPO_URL
 mkdir -p tmp
 cd ./tmp
-git clone $AUTHOR_REPO_URL
+git clone $SURVEYRUNNER_REPO_URL
 cd ./eq-author
 for branch in `git branch -a | grep remotes | grep -v HEAD | grep -v master `; do
    git branch --track ${branch#remotes/origin/} $branch
