73f4df9884b449fb91c51299fe39c7ef32b37f07
Lewis Babbage <Lewis@Lewiss-MacBook-Pro.local>
2016-02-18 12:30:32 +0000

    eq-31 smoke test with terraform

---
+++ smoke_test.tf
@@ -0,0 +1,11 @@
+resource "null_resource" "smoke_test" {
+    depends_on = ["aws_elastic_beanstalk_environment.sr_prime","aws_route53_record.survey_runner"]
+
+    provisioner "local-exec" {
+      command = "./run_smoke_test.sh ${var.env}"
+    }
+
+    provisioner "local-exec" {
+      command = "rm -rf tmp"
+    }
+
