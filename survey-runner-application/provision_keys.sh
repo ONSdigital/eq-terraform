0367bb500873549069e3a65ef1cd5bae6644fd51
jonnyshaw89 <jon.shaw@jassoft.co.uk>
2017-04-11 11:58:22 +0100

    Change folder name

--- survey-runner-application/provision_keys.sh
+++ survey-runner-application/provision_keys.sh
@@ -23,23 +23,23 @@ credstash --table $table put -k $key "EQ_SERVER_SIDE_STORAGE_DATABASE_PASSWORD" 
 credstash --table $table put -k $key "EQ_RABBITMQ_USERNAME" $rabbit_user -a
 credstash --table $table put -k $key "EQ_RABBITMQ_PASSWORD" $rabbit_pass -a
 
-credstash --table $table put -k $key "EQ_USER_AUTHENTICATION_RRM_PUBLIC_KEY" "@jwt-test-keys/sdc-user-authentication-signing-rrm-public-key.pem" -a
-credstash --table $table put -k $key "EQ_USER_AUTHENTICATION_SR_PRIVATE_KEY" "@jwt-test-keys/sdc-user-authentication-encryption-sr-private-key.pem" -a
-credstash --table $table put -k $key "EQ_USER_AUTHENTICATION_SR_PRIVATE_KEY_PASSWORD" "@jwt-test-keys/sdc-user-authentication-encryption-sr-private-key-password.txt" -a
+credstash --table $table put -k $key "EQ_USER_AUTHENTICATION_RRM_PUBLIC_KEY" "@secrets/sdc-user-authentication-signing-rrm-public-key.pem" -a
+credstash --table $table put -k $key "EQ_USER_AUTHENTICATION_SR_PRIVATE_KEY" "@secrets/sdc-user-authentication-encryption-sr-private-key.pem" -a
+credstash --table $table put -k $key "EQ_USER_AUTHENTICATION_SR_PRIVATE_KEY_PASSWORD" "@secrets/sdc-user-authentication-encryption-sr-private-key-password.txt" -a
 
-credstash --table $table put -k $key "EQ_SUBMISSION_SDX_PUBLIC_KEY" "@jwt-test-keys/sdc-submission-encryption-sdx-public-key.pem" -a
-credstash --table $table put -k $key "EQ_SUBMISSION_SR_PRIVATE_SIGNING_KEY" "@jwt-test-keys/sdc-submission-signing-sr-private-key.pem" -a
-credstash --table $table put -k $key "EQ_SUBMISSION_SR_PRIVATE_SIGNING_KEY_PASSWORD" "@jwt-test-keys/sdc-submission-signing-sr-private-key-password.txt" -a
+credstash --table $table put -k $key "EQ_SUBMISSION_SDX_PUBLIC_KEY" "@secrets/sdc-submission-encryption-sdx-public-key.pem" -a
+credstash --table $table put -k $key "EQ_SUBMISSION_SR_PRIVATE_SIGNING_KEY" "@secrets/sdc-submission-signing-sr-private-key.pem" -a
+credstash --table $table put -k $key "EQ_SUBMISSION_SR_PRIVATE_SIGNING_KEY_PASSWORD" "@secrets/sdc-submission-signing-sr-private-key-password.txt" -a
 
 #DEV MODE only
-credstash --table $table put -k $key "EQ_USER_AUTHENTICATION_SR_PUBLIC_KEY" "@jwt-test-keys/sdc-submission-signing-sr-public-key.pem" -a
-credstash --table $table put -k $key "EQ_USER_AUTHENTICATION_RRM_PRIVATE_KEY" "@jwt-test-keys/sdc-user-authentication-signing-rrm-private-key.pem" -a
-credstash --table $table put -k $key "EQ_USER_AUTHENTICATION_RRM_PRIVATE_KEY_PASSWORD" "@jwt-test-keys/sdc-user-authentication-signing-rrm-private-key-password.txt" -a
+credstash --table $table put -k $key "EQ_USER_AUTHENTICATION_SR_PUBLIC_KEY" "@secrets/sdc-submission-signing-sr-public-key.pem" -a
+credstash --table $table put -k $key "EQ_USER_AUTHENTICATION_RRM_PRIVATE_KEY" "@secrets/sdc-user-authentication-signing-rrm-private-key.pem" -a
+credstash --table $table put -k $key "EQ_USER_AUTHENTICATION_RRM_PRIVATE_KEY_PASSWORD" "@secrets/sdc-user-authentication-signing-rrm-private-key-password.txt" -a
 
 
-credstash --table $table put -k $key "EQ_SERVER_SIDE_STORAGE_USER_ID_SALT" "@jwt-test-keys/sdc-storage-user-id-salt.txt"
-credstash --table $table put -k $key "EQ_SERVER_SIDE_STORAGE_USER_IK_SALT" "@jwt-test-keys/sdc-storage-user-ik-salt.txt"
-credstash --table $table put -k $key "EQ_SERVER_SIDE_STORAGE_ENCRYPTION_USER_PEPPER" "@jwt-test-keys/sdc-storage-encryption-user-pepper.txt"
+credstash --table $table put -k $key "EQ_SERVER_SIDE_STORAGE_USER_ID_SALT" "@secrets/sdc-storage-user-id-salt.txt"
+credstash --table $table put -k $key "EQ_SERVER_SIDE_STORAGE_USER_IK_SALT" "@secrets/sdc-storage-user-ik-salt.txt"
+credstash --table $table put -k $key "EQ_SERVER_SIDE_STORAGE_ENCRYPTION_USER_PEPPER" "@secrets/sdc-storage-encryption-user-pepper.txt"
 
 deactivate
 rmvirtualenv credstash
