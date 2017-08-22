d003432561561f7996f6b231d01d52436f5bc06b
Jon Shaw <jonny.shaw@jassoft.co.uk>
2017-08-22 12:42:32 +0100

    Fixed ansible run issue when hostname has trailing dot (#95)
    * Fixed ansible run issue when hostname has trailing dot
    
    * Enable Detailed Monitoring
    
    * Remove duplicated alarm and use count
    
    * Treat missing data as breaching
    
    * Tidy up Alerting
    
    * Increase cluster status alarm to 2 evaluation_periods
    
    * Removed control_path from Ansible config
    
    It was causing a '[string] too long for unix domain socket'
    error. After some googling it turns out control path was often used as
    a workaround for this error but now a proper fix has gone in and
    having it can actually cause the error. The recommendation is to
    remove it which worked for me. See the end of https://github.com/ansible/ansible/issues/11536.
    
    * Opened Port for SDX
    
    * Disabled Ansible host key checking
    
    Added the environment variable to the front of the playbook command.
    
    * Remove ansible.cfg as it did nothing

--- survey-runner-queue/cloudwatch.tf
+++ survey-runner-queue/cloudwatch.tf
@@ -1,29 +1,16 @@
-resource "aws_cloudwatch_metric_alarm" "rabbitmq1_cluster_status" {
-  alarm_name          = "${var.env}-rabbitmq1-cluster-alert"
-  evaluation_periods  = "1"
-  comparison_operator = "GreaterThanOrEqualToThreshold"
-  metric_name         = "${var.env}-rabbitmq1"
-  namespace           = "RabbitMQClusterStatus"
-  period              = "60"
-  statistic           = "Average"
-  threshold           = "1"
-  alarm_description   = "${var.env} rabbit mq 1 reporting cluster paritions"
-  alarm_actions       = ["arn:aws:sns:eu-west-1:${data.aws_caller_identity.current.account_id}:${var.env}-slack-alert"]
-  insufficient_data_actions = ["arn:aws:sns:eu-west-1:${data.aws_caller_identity.current.account_id}:${var.env}-slack-alert"]
-}
-
-resource "aws_cloudwatch_metric_alarm" "rabbitmq2_cluster_status" {
-  alarm_name          = "${var.env}-rabbitmq2-cluster-alert"
-  evaluation_periods  = "1"
-  comparison_operator = "GreaterThanOrEqualToThreshold"
-  metric_name         = "${var.env}-rabbitmq2"
-  namespace           = "RabbitMQClusterStatus"
-  period              = "60"
-  statistic           = "Average"
-  threshold           = "1"
-  alarm_description   = "${var.env} rabbit mq 2 reporting cluster paritions"
-  alarm_actions       = ["arn:aws:sns:eu-west-1:${data.aws_caller_identity.current.account_id}:${var.env}-slack-alert"]
-  insufficient_data_actions = ["arn:aws:sns:eu-west-1:${data.aws_caller_identity.current.account_id}:${var.env}-slack-alert"]
+resource "aws_cloudwatch_metric_alarm" "rabbitmq_cluster_status" {
+  count                     = 2
+  alarm_name                = "${var.env}-rabbitmq${count.index + 1}-cluster-alert"
+  evaluation_periods        = "2"
+  comparison_operator       = "GreaterThanOrEqualToThreshold"
+  metric_name               = "${var.env}-rabbitmq${count.index + 1}"
+  namespace                 = "RabbitMQClusterStatus"
+  period                    = "60"
+  statistic                 = "Maximum"
+  threshold                 = "1"
+  alarm_description         = "${var.env} rabbit mq ${count.index + 1} reporting cluster paritions"
+  alarm_actions             = ["arn:aws:sns:eu-west-1:${data.aws_caller_identity.current.account_id}:${var.env}-slack-alert"]
+  treat_missing_data        = "breaching"
 }
 
 resource "aws_cloudwatch_metric_alarm" "rabbitmq_cpu" {
@@ -45,18 +32,17 @@ resource "aws_cloudwatch_metric_alarm" "rabbitmq_cpu" {
 }
 
 resource "aws_cloudwatch_metric_alarm" "rabbitmq_status" {
-  count               = 2
-  alarm_name          = "${var.env}-rabbitmq-${count.index + 1}-status-alert"
-  evaluation_periods  = "1"
-  comparison_operator = "GreaterThanOrEqualToThreshold"
-  metric_name         = "StatusCheckFailed"
-  namespace           = "AWS/EC2"
-  period              = "60"
-  statistic           = "Maximum"
-  threshold           = "1"
-  alarm_description   = "Alert generated if status changes to failure"
-  alarm_actions       = ["arn:aws:sns:eu-west-1:${data.aws_caller_identity.current.account_id}:${var.env}-slack-alert"]
-  insufficient_data_actions = ["arn:aws:sns:eu-west-1:${data.aws_caller_identity.current.account_id}:${var.env}-slack-alert"]
+  count                     = 2
+  alarm_name                = "${var.env}-rabbitmq${count.index + 1}-status-alert"
+  evaluation_periods        = "1"
+  comparison_operator       = "GreaterThanOrEqualToThreshold"
+  metric_name               = "StatusCheckFailed"
+  namespace                 = "AWS/EC2"
+  period                    = "60"
+  statistic                 = "Maximum"
+  threshold                 = "1"
+  alarm_description         = "Alert generated if status changes to failure"
+  alarm_actions             = ["arn:aws:sns:eu-west-1:${data.aws_caller_identity.current.account_id}:${var.env}-slack-alert"]
 
   dimensions {
     "InstanceId" = "${element(aws_instance.rabbitmq.*.id,count.index)}"
--- survey-runner-queue/message_queue.tf
+++ survey-runner-queue/message_queue.tf
@@ -7,6 +7,7 @@ resource "aws_instance" "rabbitmq" {
   subnet_id                   = "${aws_subnet.queue.id}"
   private_ip                  = "${var.rabbitmq_ips[count.index]}"
   associate_public_ip_address = true
+  monitoring                  = true
 
   vpc_security_group_ids = ["${aws_security_group.rabbit_required.id}",
     "${aws_security_group.provision-allow-ssh-REMOVE.id}",
@@ -15,12 +16,12 @@ resource "aws_instance" "rabbitmq" {
   ]
 
   root_block_device {
-    volume_type = "gp2"
+    volume_type           = "gp2"
     delete_on_termination = "${var.delete_volume_on_termination}"
   }
 
   tags {
-    Name = "${var.env}-rabbitmq-${count.index + 1}"
+    Name    = "${var.env}-rabbitmq-${count.index + 1}"
     Service = "${var.env}-RabbitMQ"
   }
 }
@@ -29,10 +30,11 @@ data "template_file" "hosts" {
   template = "${file("${path.module}/templates/hosts")}"
 
   vars = {
-    rabbitmq1_ip = "${aws_instance.rabbitmq.0.private_ip}"
-    rabbitmq2_ip = "${aws_instance.rabbitmq.1.private_ip}"
-    deploy_env   = "${var.env}"
-    deploy_dns   = "${data.aws_route53_zone.dns_zone.name}"
+    rabbitmq1_ip   = "${aws_instance.rabbitmq.0.private_ip}"
+    rabbitmq2_ip   = "${aws_instance.rabbitmq.1.private_ip}"
+    rabbitmq1_fqdn = "${element(aws_route53_record.rabbitmq.*.fqdn, 1)}"
+    rabbitmq2_fqdn = "${element(aws_route53_record.rabbitmq.*.fqdn, 2)}"
+    deploy_env     = "${var.env}"
   }
 }
 
@@ -116,7 +118,7 @@ resource "null_resource" "ansible" {
   }
 
   provisioner "local-exec" {
-    command = "ansible-playbook -i '${var.env}-rabbitmq1.${data.aws_route53_zone.dns_zone.name},${var.env}-rabbitmq2.${data.aws_route53_zone.dns_zone.name}'  --private-key ${var.aws_key_pair}.pem tmp/eq-messaging/ansible/rabbitmq-cluster.yml --extra-vars '{\"deploy_env\":\"${var.env}\",\"deploy_dns\":\"${var.dns_zone_name}\",\"rabbitmq_admin_user\":\"${var.rabbitmq_admin_user}\",\"rabbitmq_admin_password\":\"${var.rabbitmq_admin_password}\",\"rabbitmq_write_user\":\"${var.rabbitmq_write_user}\",\"rabbitmq_write_password\":\"${var.rabbitmq_write_password}\",\"rabbitmq_read_user\":\"${var.rabbitmq_read_user}\",\"rabbitmq_read_password\":\"${var.rabbitmq_read_password}\", \"rsyslogd_server_IP\":\"${var.rsyslogd_server_ip}\", \"region\":\"${var.aws_default_region}\"}'"
+    command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i '${element(aws_route53_record.rabbitmq.*.fqdn, 1)},${element(aws_route53_record.rabbitmq.*.fqdn, 2)}'  --private-key ${var.aws_key_pair}.pem tmp/eq-messaging/ansible/rabbitmq-cluster.yml --extra-vars '{\"deploy_env\":\"${var.env}\",\"deploy_dns\":\"${var.dns_zone_name}\",\"rabbitmq_admin_user\":\"${var.rabbitmq_admin_user}\",\"rabbitmq_admin_password\":\"${var.rabbitmq_admin_password}\",\"rabbitmq_write_user\":\"${var.rabbitmq_write_user}\",\"rabbitmq_write_password\":\"${var.rabbitmq_write_password}\",\"rabbitmq_read_user\":\"${var.rabbitmq_read_user}\",\"rabbitmq_read_password\":\"${var.rabbitmq_read_password}\", \"rsyslogd_server_IP\":\"${var.rsyslogd_server_ip}\", \"region\":\"${var.aws_default_region}\"}'"
   }
 
   provisioner "local-exec" {
