ee31693f49ed0bff5fc93fe063201502463c1076
Jon Shaw <jon.shaw@jassoft.co.uk>
2017-10-20 15:59:42 +0200

    Pass the NAT gateway IPs into the ECS module (#105)

--- survey-runner-routing/outputs.tf
+++ survey-runner-routing/outputs.tf
@@ -5,3 +5,7 @@ output "private_route_table_ids" {
 output "public_subnet_ids" {
   value = ["${aws_subnet.public.*.id}"]
 }
+
+output "nat_gateway_ips" {
+  value = "${aws_eip.nat_eip.*.public_ip}"
+}
