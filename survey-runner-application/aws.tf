0858ec08c4e04636269d1f131153bc547543b203
jonnyshaw89 <jon.shaw@jassoft.co.uk>
2018-06-12 16:27:08 +0100

    Add Terraform backend config This allows for the modules to be run independently without having to inject this config (which is how Jenkins currently does it)

--- survey-runner-application/aws.tf
+++ survey-runner-application/aws.tf
@@ -13,3 +13,11 @@ data "aws_region" "current" {
 data "aws_route53_zone" "dns_zone" {
   name = "${var.dns_zone_name}"
 }
+
+terraform {
+  required_version = ">= 0.10.0, < 0.11.0"
+
+  backend "s3" {
+    region = "eu-west-1"
+  }
+}
