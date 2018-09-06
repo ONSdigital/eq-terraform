33b6cfe55a94040fc3d5939c09f1a55ddd5ac309
Samiwel Thomas <samiwel.thomas@gmail.com>
2018-09-06 09:20:33 +0100

    Update lambda runtime to nodejs 8.10 LTS.

--- survey-runner-alerting/lambda.tf
+++ survey-runner-alerting/lambda.tf
@@ -9,7 +9,7 @@ resource "aws_lambda_function" "slack_alert" {
   function_name = "${var.env}-slack-alert"
   role = "${aws_iam_role.iam_for_slack_alert.arn}"
   handler = "index.handler"
-  runtime = "nodejs4.3"
+  runtime = "nodejs8.10"
   timeout = "3"
   source_code_hash = "${data.archive_file.slack_alert_zip.output_base64sha256}"
   environment {
