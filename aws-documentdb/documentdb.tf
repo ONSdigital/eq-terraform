ace5dd2d7b2ede4fec1b841bf16e900ddcf45ad2
martyncolmer <martyc1974@hotmail.com>
2020-12-14 13:04:06 +0000

    updated ref

--- aws-documentdb/documentdb.tf
+++ aws-documentdb/documentdb.tf
@@ -43,6 +43,6 @@ resource "aws_docdb_cluster" "docdb" {
 resource "aws_docdb_cluster_instance" "cluster_instances" {
   count              = 3
   identifier         = "${var.env}-${var.documentdb_cluster_name}-cluster-instance-${count.index}"
-  cluster_identifier = aws_docdb_cluster.default.id
+  cluster_identifier = "${aws_docdb_cluster.default.id}"
   instance_class     = "db.t3.medium"
 }
