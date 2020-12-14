d44770810ca9e36a4fd27cbd8011fde260cae9ab
martyncolmer <martyc1974@hotmail.com>
2020-12-14 09:48:19 +0000

    updated outputs

--- aws-docmentdb/outputs.tf
+++ aws-docmentdb/outputs.tf
@@ -1,16 +1,3 @@
-output "vpc_id" {
-  value = "${aws_vpc.survey_runner.id}"
-}
-
-output "internet_gateway_id" {
-  value = "${aws_internet_gateway.igw.id}"
-}
-
-output "database_subnet_ids" {
-  value = ["${aws_subnet.database.*.id}"]
-}
-
-
-output "database_subnet_group_name" {
-  value = "${aws_db_subnet_group.database_subnet.name}"
+output "cluster_identifier" {
+  value = "${aws_docdb_cluster.docdb.cluster_identifier}"
 }
